36bd719934f83fbf5a07f1b3497c34bc4b4c9282
Ariel Kollross <arielkollross@gmail.com>
2020-05-02 20:13:12 -0300

    scan QRcode to add something

--- Mobile/src/pages/QRcode/index.js
+++ Mobile/src/pages/QRcode/index.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect} from 'react'
 import {Text, View, StyleSheet, Button} from 'react-native'
-import {CAMERA} from 'expo-permissions'
 import {BarCodeScanner} from 'expo-barcode-scanner'
  
 export default function QrCodeBuy(){
     const [hasPermission, setHasPermission] = useState(null);
     const [scanned, setScanned] = useState(false);
+    const [AddToShopCart, setAddToShopCart] =useState([])
   
     useEffect(() => {
       (async () => {
@@ -16,7 +16,18 @@ export default function QrCodeBuy(){
   
     const handleBarCodeScanned = ({ type, data }) => {
       setScanned(true);
-      alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+      //alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+      
+      if(type == 256){
+        AddToShopCart.push({
+          id: 2,
+          name: "Manga One Punch Man", 
+          price: 20.99 ,
+        })
+        alert(AddToShopCart)
+        alert(`Add new item to shop cart item: ${AddToShopCart}`)
+      }
+        
     };
   
     if (hasPermission === null) {
